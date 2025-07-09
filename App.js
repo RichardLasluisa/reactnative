c1337792f319dae1f51d28c5244275582cc3f671
rlasluisa <richardlasluisa@gmail.com>
2025-07-09 18:22:38 -0500

    primera carga

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, Button, Pressable } from 'react-native';
+import { icon } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+
+      <Text>Hola Mundo</Text>
+      <Image style={styles.image} source={icon}></Image>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Pulsame" color="#841584"/>
+        <Pressable
+          onPress={() => {
+            setTimesPressed(current => current + 1);
+          }}
+          style={({pressed}) => [
+            {
+              backgroundColor: pressed ? 'rgb(210, 100, 255)' : 'white',
+            },
+            styles.wrapperCustom,
+          ]}>
+          {({pressed}) => (
+            <Text style={styles.text}>{pressed ? 'Pressed!' : 'Press Me'}</Text>
+          )}
+        </Pressable>
+
+      
     </View>
   );
 }
@@ -17,4 +38,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 430,
+    height: 294,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+
+
 });
